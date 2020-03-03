5f5da3081aa28b7bf49dddff5ae74452b946bb28
StormLok953 <tyler.ys.kim@gmail.com>
2020-03-02 22:28:28 -0500

    temporary

--- Competition/src/TylerK_C3Intermediate_Veitch2020.java
+++ Competition/src/TylerK_C3Intermediate_Veitch2020.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.io.FileNotFoundException;
 
-public class TylerKC3JaVeitch2020 {
+public class TylerK_C3Intermediate_Veitch2020 {
 
     public static void main(String args[]) throws FileNotFoundException {
         //initialization
