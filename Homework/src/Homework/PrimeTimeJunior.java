be6cdade0213e25dd5a7b3831489494835af40c6
StormLok953 <tyler.ys.kim@gmail.com>
2020-04-11 12:38:10 -0400

    Prime Time Finish

--- Homework/src/Homework/PrimeTimeJunior.java
+++ Homework/src/Homework/PrimeTimeJunior.java
@@ -1,15 +1,106 @@
 package Homework;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PrimeTimeJunior {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws FileNotFoundException {
 
-        String input = "17";
+        Scanner input = new Scanner(new File("C:\\Users\\tyler\\dev\\TylerKimJavaHomework\\Homework\\src\\Homework\\PrimeTimeJuniorInput"));
 
-        System.out.println(isPrime(Integer.parseInt(input)));
+      while(input.hasNext()) {
+          ArrayList<String> output = new ArrayList<>();
+          String inputLine = input.nextLine();
 
+          //compute final
+          if(isCircular(inputLine)) {
+              output.add("Circular");
+          }
+
+          if(isLeft(inputLine)) {
+              output.add("Left");
+          }
+
+          if(isRight(inputLine)) {
+              output.add("Right");
+          }
+
+          if(isInterior(inputLine)) {
+              output.add("Interior");
+          }
+
+          if(output.size() == 0) {
+              output.add("NONE");
+          }
+
+          System.out.println(output);
+      }
+
+
+
+    }
+
+    //executing methods
+        //circular
+    public static boolean isCircular(String input) {
+        int tempValue = rotate(input);
+
+
+
+        if(isPrime(tempValue)) {
+            return true;
+        }
+
+
+        for(int i = 0; i < input.length(); i++) {
+            tempValue = rotate(Integer.toString(tempValue));
+
+            if(!isPrime(tempValue)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+        //left
+    public static boolean isLeft(String input) {
+        if(isPrime(leftMost(input))) {
+            return true;
+        }
+        return false;
+    }
+
+        //right
+    public static boolean isRight(String input) {
+        if(isPrime(rightMost(input)))  {
+            return true;
+        }
+        return false;
+    }
+
+        //interior
+    public static boolean isInterior(String input) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 1; i < input.length()-1; i++) {
+
+            for(int j = 0; j < input.length(); j++) {
+                if(i != j) {
+                    stringBuilder.append(input.substring(j, j+1));
+                    if(isPrime(Integer.parseInt(stringBuilder.toString()))) {
+                        return true;
+                    }
+                }
+            }
+
+        }
+
+        return false;
     }
 
 
@@ -17,6 +108,10 @@ public class PrimeTimeJunior {
     //supplementary methods
         //prime number finder
     public static boolean isPrime(int number) {
+
+        if(number == 1) {
+            return false;
+        }
 
         for(int i = 2; i < number; i++) {
             if(number % i == 0) {
@@ -29,40 +124,42 @@ public class PrimeTimeJunior {
 
         //rotate numbers
     public static int rotate(String number) {
-        ArrayList<String> numberToArray = new ArrayList<>();
-        ArrayList<String> output = new ArrayList<>();
-        int x = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        String tempValue = number.substring(0, 1);
 
-        //add first element
-        output.add(numberToArray.get(0));
-
-        for(int i = 1; i < numberToArray.size(); i++) {
-            output.add(0, numberToArray.get(i));
+        for(int i = 1; i < number.length(); i++) {
+            stringBuilder.append(number.substring(i, i+1));
         }
 
-        output = inverseArr(output);
+        stringBuilder.append(tempValue);
+        String finalString = stringBuilder.toString();
 
-        //make new int
-        for(int i = 0; i < output.size(); i++) {
-
-            x += Integer.parseInt(output.get(i)) + ((i+1) * 10);
-
-
-        }
-
-        return x;
+        return Integer.parseInt(finalString);
 
     }
 
-        //inverse arraylist
-    public static ArrayList<String> inverseArr(ArrayList<String> input) {
-        ArrayList<String> inverseOutput = new ArrayList<>();
+        //left delete
+    public static int rightMost(String number) {
+        StringBuilder stringBuilder = new StringBuilder();
 
-        for(int i = input.size(); i >= 0; i++) {
-            inverseOutput.add(input.get(i));
+        for(int i = 0; i < number.length()-1; i++) {
+            stringBuilder.append(number.substring(i, i+1));
         }
 
-        return inverseOutput;
+        String numberString = stringBuilder.toString();
+        return Integer.parseInt(numberString);
+    }
+
+        //right delete
+    public static int leftMost(String number) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 1; i < number.length(); i++) {
+            stringBuilder.append(number.substring(i, i+1));
+        }
+
+        String numberString = stringBuilder.toString();
+        return Integer.parseInt(numberString);
     }
 
 }
